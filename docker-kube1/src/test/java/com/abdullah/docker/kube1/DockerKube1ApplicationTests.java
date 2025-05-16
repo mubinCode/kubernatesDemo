5cd3464117aa65d93026b8d42d5a9c7efaa30148
mubinCode <amubineee@gmail.com>
2025-05-16 22:58:59 +0600

    first commit

---
+++ docker-kube1/src/test/java/com/abdullah/docker/kube1/DockerKube1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdullah.docker.kube1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerKube1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
